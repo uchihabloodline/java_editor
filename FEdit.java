af06ce45c903c65e46a52023be5c5a88e996706f
shivam <geetapandey595@gmail.com>
2017-12-25 19:29:25 +0530

    editor file

---
+++ FEdit.java
@@ -0,0 +1,28 @@
+/**
+ * @name        Simple Java NotePad
+ * @package     ph.notepad
+ * @file        UI.java
+ * @email       geetapandey595@gmail.com
+ * @link        https://github.com/uchihabloodline
+ * 
+ * @license     Apache (http://www.apache.org/licenses/LICENSE-2.0)
+ * @create      2017-08-20
+ * @update      2015-12-04
+ *
+ *
+ * @modifiedby  shivam pandey
+ * @modweb      https://github.com/uchihabloodline
+ * @modemail    geetapandey595@gmail.com
+ */
+
+package simplejavatexteditor;
+
+import javax.swing.JTextArea;
+
+public class FEdit {
+
+    public static void clear(JTextArea textArea) {
+        textArea.setText("");
+    }
+
+}
